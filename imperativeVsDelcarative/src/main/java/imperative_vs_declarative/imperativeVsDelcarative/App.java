131071380ac381600e4a947884d050945f1a4fb1
kupender <karnatikrishnaiah71@gmail.com>
2026-03-12 10:02:49 +0530

    first practice on imperative and delcarative programming

---
+++ imperativeVsDelcarative/src/main/java/imperative_vs_declarative/imperativeVsDelcarative/App.java
@@ -0,0 +1,10 @@
+package imperative_vs_declarative.imperativeVsDelcarative;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
